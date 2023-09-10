f39be86e8e90831480d148615905de487b4b7404
Eon <eoneon123@gmail.com>
2023-09-09 18:10:01 -0700

    added and set up devise; last commit before adding pundit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount ImageUploader.derivation_endpoint => "/derivations/image"
   resources :categories
   resources :photos
   resources :search_photos
   resources :sort_photos
+
   root "photos#index"
 end
